92616faf69eabc1d7f77cf3f8f191e79ee3bd147
João Victor Abreu dos Santos <jvictorabreu99@gmail.com>
2022-10-08 16:38:41 -0300

    Adicionando métodos getters and setters

--- src/Caneta.java
+++ src/Caneta.java
@@ -2,22 +2,10 @@ public class Caneta {
     public static void main(String[] args) {
         // Instanciando objeto caneta 1.
         Pen c1 = new Pen();
-        c1.modelo = "Bic Cristal";
-        c1.cor = "Azul";
-//        c1.ponta = 0.5f;
-//        c1.carga = 80;
-//        c1.tampada = false;
-        c1.destampar();
+        c1.setModelo("Bic");
+        c1.setPonta(0.5f);
 //        c1.status();
-        c1.escrever();
-
-        System.out.println("");
-
-        Pen c2 = new Pen();
-        c2.modelo = "Faber Castel";
-        c2.cor = "Vermelha";
-        c2.tampar();
-        c2.status();
-        c2.escrever();
+        c1.setTampada(true);
+        System.out.println("Está é uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta() + ".");
     }
 }
--- src/Pen.java
+++ src/Pen.java
@@ -1,18 +1,62 @@
 // Criando os atributos e métodos da classe Pen.
 public class Pen {
-    public String modelo;
-    public String cor;
+    private String modelo;
+    private String cor;
     private float ponta;
     private float carga = 100;
     private boolean tampada;
 
+    // Criando método Construtor que deve ter o mesmo nome da Classe.
+    public void Pen(){
+
+    }
+
+    //Setter and getter para Modelo
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+
+    //Setter and getter para Cor
+    public String getCor(){
+        return this.cor;
+    }
+    public void setCor(String c){
+        this.cor = c;
+    }
+
+    //Setter and getter para Ponta
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+
+    //Setter and getter para Carga
+    public float getCarga(){
+        return this.carga;
+    }
+    public void setCarga(float ca){
+        this.carga = ca;
+    }
+
+    //Setter and getter para Tampada
+    public boolean getTampada(){
+        return this.tampada;
+    }
+    public void setTampada(Boolean t){
+        this.tampada = t;
+    }
 
     public void status(){
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Uma caneta " + this.cor);
-//        System.out.println("Ponta: " + this.ponta);
-        System.out.println("Carga: " + this.carga);
-        System.out.println("Está tampada?: " + this.tampada);
+        System.out.println("Modelo: " + this.getModelo());
+//        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.getPonta());
+//        System.out.println("Carga: " + this.carga);
+//        System.out.println("Está tampada?: " + this.tampada);
     }
 
     public void escrever(){
